e461fd8b0aa53aa536199bd21ea5022cd1844277
Hang Xiao <xiaoh10@janelia.hhmi.org>
2011-06-19 17:12:14 -0400

    add opengl nehe scripts

--- create_code_opengl.sh
+++ create_code_opengl.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-if [ "$#" = 0 ]; then
+if [ "$#" = "0" ]; then
 	echo "usage : create_code_opengl.sh <code_name> [option] <os_type>"
 	echo "Use \"create_code_opengl.sh !\" to display the detailed information."
 	exit 0
@@ -12,6 +12,7 @@ if [ "$1" = "!" ]; then
 	echo ""
 	echo "nehe includes many opengl course, the option is the lesson's number."
 	if [ "1" ] ; then
+		echo "01 #   An OpenGL Window"
 		echo "02 #   First Polygon"
 		echo "03 #   Adding Color"
 		echo "04 #   Rotation"
@@ -19,5 +20,37 @@ if [ "$1" = "!" ]; then
 		echo "06 #   Texture Mapping"
 		echo "07 #   Texture Filters, Lighting & Keyboard Control"
 	fi | column -t -s \#
+	exit 0
 fi
 
+CODE_TYPE=$1
+CODE_NAME=$2
+OS_TYPE=$3
+GC_PATH=`which getcode.sh`
+OPENGL_SCRIPT_PATH="`dirname $GC_PATH`/opengl"
+
+if [ "$OS_TYPE" = "" ]; then 
+	uname=`uname`
+	if [ "$uname" = "Darwin" ]; then
+		OS_TYPE="macos"
+	else
+		OS_TYPE="linux"
+	fi
+fi
+
+if [ "$1" = "nehe" ]; then
+	if [[ "$CODE_NAME" = "" || "$CODE_NAME" = "macos" || "$CODE_NAME" = "linux" || "$OS_TYPE" = "" ]]; then
+		if [ "1" ] ; then
+			echo "01 #   An OpenGL Window"
+			echo "02 #   First Polygon"
+			echo "03 #   Adding Color"
+			echo "04 #   Rotation"
+			echo "05 #   3D Shapes"
+			echo "06 #   Texture Mapping"
+			echo "07 #   Texture Filters, Lighting & Keyboard Control"
+		fi | column -t -s \#
+	else
+		eval "$OPENGL_SCRIPT_PATH/${CODE_TYPE}_${CODE_NAME}_${OS_TYPE}.sh"
+	fi
+elif [ "$1" = "" ]; then :
+fi
