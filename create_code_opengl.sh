6a72ddc98d0f49d93a37f36b9ebddd36257c5a0b
Hang Xiao <xiaoh10@janelia.hhmi.org>
2011-06-23 00:32:51 -0400

    add glwidget

--- create_code_opengl.sh
+++ create_code_opengl.sh
@@ -1,18 +1,28 @@
 #! /bin/sh
 
 if [[ "$#" = "0" || "$1" = "!" ]]; then
-	echo "usage : create_code_opengl.sh <code_name> [option] <os_type>"
+	echo "usage : create_code_opengl.sh <code_name> <os_type>"
 	echo ""
 	echo -e "The command will produce opengl codes. The os_type is macos or linux. If omited, it will be current os type."
 	echo ""
-	echo -e "Available Codes : smoke nehe qt"
+	echo -e "Available Codes :"
+		if [ "1" ] ; then
+			echo "smoke    #   normal opengl test"
+			echo "qt-smoke #   qt smoke test"
+			echo "glwidget #   very simple QGLWidget implementation"
+			echo "nehe01   #   An OpenGL Window"
+			echo "nehe02   #   First Polygon"
+			echo "nehe03   #   Adding Color"
+			echo "nehe04   #   Rotation"
+			echo "nehe05   #   3D Shapes"
+			echo "nehe06   #   Texture Mapping"
+		fi | column -t -s \#
 	echo ""
 	exit 0
 fi
 
-CODE_TYPE=$1
-CODE_NAME=$2
-OS_TYPE=$3
+CODE_NAME=$1
+OS_TYPE=$2
 GC_PATH=`which getcode.sh`
 OPENGL_SCRIPT_PATH="`dirname $GC_PATH`/opengl"
 
@@ -25,37 +35,4 @@ if [ "$OS_TYPE" = "" ]; then
 	fi
 fi
 
-if [ "$1" = "nehe" ]; then
-	if [[ "$CODE_NAME" = "" || "$CODE_NAME" = "macos" || "$CODE_NAME" = "linux" || "$OS_TYPE" = "" ]]; then
-		echo "Available Codes :"
-		if [ "1" ] ; then
-			echo "01 #   An OpenGL Window"
-			echo "02 #   First Polygon"
-			echo "03 #   Adding Color"
-			echo "04 #   Rotation"
-			echo "05 #   3D Shapes"
-			echo "06 #   Texture Mapping"
-		fi | column -t -s \#
-	else
-		eval "$OPENGL_SCRIPT_PATH/${CODE_TYPE}_${CODE_NAME}_${OS_TYPE}.sh"
-	fi
-elif [ "$1" = "smoke" ]; then
-	if [[ "$CODE_NAME" = "" || "$CODE_NAME" = "macos" || "$CODE_NAME" = "linux" || "$OS_TYPE" = "" ]]; then
-		echo "Available Codes :"
-		if [ "1" ]; then
-			echo "framework #   produce opengl smoke program based on framework"
-			echo "x11 #   produce opengl smoke program based on x11"
-		fi | column -t -s \#
-	else
-		eval "$OPENGL_SCRIPT_PATH/${CODE_TYPE}_${CODE_NAME}_${OS_TYPE}.sh"
-	fi
-elif [ "$1" = "qt" ]; then
-	if [[ "$CODE_NAME" = "" || "$CODE_NAME" = "macos" || "$CODE_NAME" = "linux" || "$OS_TYPE" = "" ]]; then
-		echo "Available Codes :"
-		if [ "1" ]; then
-			echo "smoke #   produce qt smoke program"
-		fi | column -t -s \#
-	else
-		eval "$OPENGL_SCRIPT_PATH/${CODE_TYPE}_${CODE_NAME}_${OS_TYPE}.sh"
-	fi
-fi
+eval "$OPENGL_SCRIPT_PATH/${CODE_NAME}_${OS_TYPE}.sh"
--- opengl/create_opengl_shell_codes.sh
+++ opengl/create_opengl_shell_codes.sh
@@ -2,14 +2,14 @@ cp /Users/xiaoh10/Sites/Sites/research/tools/opengl/nehe_courses/macos_nehe/make
 for i in {1..6}
 do
 	cp /Users/xiaoh10/Sites/Sites/research/tools/opengl/nehe_courses/macos_nehe/lesson$i.c ./
-	to_echo_script.sh lesson$i.c makefile > nehe_0${i}_macos.sh
-	chmod +x nehe_0${i}_macos.sh
+	to_echo_script.sh lesson$i.c makefile > nehe0${i}_macos.sh
+	chmod +x nehe0${i}_macos.sh
 done
 
 cp /Users/xiaoh10/Sites/Sites/research/tools/opengl/nehe_courses/linux_nehe/makefile ./
 for i in {1..6}
 do
 	cp /Users/xiaoh10/Sites/Sites/research/tools/opengl/nehe_courses/linux_nehe/lesson$i.c ./
-	to_echo_script.sh lesson$i.c makefile > nehe_0${i}_linux.sh
-	chmod +x nehe_0${i}_linux.sh
+	to_echo_script.sh lesson$i.c makefile > nehe0${i}_linux.sh
+	chmod +x nehe0${i}_linux.sh
 done
