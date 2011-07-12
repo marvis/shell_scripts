4e061f503ed99a845b8e1200dab9b10aa93b3c91
xiaohang <xiaohang@picb.ac.cn>
2011-07-12 15:50:48 +0800

    refresh mycode_extract_renren_music.sh

--- mycode_extract_renren_music.sh
+++ mycode_extract_renren_music.sh
@@ -1,22 +1,28 @@
-if [ "$#" = "2" ]; then
-wget --save-cookies cookies.txt --keep-session-cookies  --post-data "email=$1&password=$2" -O home -o log "http://www.renren.com/PLogin.do"
-elif [ "$#" = "1" ]; then
-wget --save-cookies cookies.txt --keep-session-cookies  --post-data "`cat $1`" -O home -o log "http://www.renren.com/PLogin.do"
-else
-echo "usage : ./extract_renren_music.sh <email> <password>"
-echo "usage : ./extract_renren_music.sh <login_file>"
-echo "cat email=xxx&password=xxx > login_file"
+if [ "$#" = "0" ]; then
+echo "usage : ./extract_renren_music.sh <email> <password> [<music_name>]"
+echo "usage : ./extract_renren_music.sh -f <login_file> [<music_name>]"
+echo "echo \"email=xxx&password=xxx\" > login_file"
 exit 0
 fi
-echo "Login renren successfully!"
-echo "get renren  musicbox ..."
-wget --load-cookies cookies.txt --save-cookies cookies.txt --keep-session-cookies -O musicbox -o log "http://music.renren.com/musicbox"
-echo "extract mp3 url ..."
-grep "*.mp3" musicbox
-mp3_src=`grep -o 'http:.*\.mp3' musicbox`
-if [ "$mp3_src" != "" ];
-then
-wget --load-cookies cookies.txt --save-cookies cookies.txt --keep-session-cookies $mp3_src
+if [ "$1" != "-f" ]; then
+wget --save-cookies cookies.txt --keep-session-cookies  --post-data "email=$1&password=$2" -O home -o log "http://www.renren.com/PLogin.do"
 else
-echo "No music link is found!"
+wget --save-cookies cookies.txt --keep-session-cookies  --post-data "`cat $2`" -O home -o log "http://www.renren.com/PLogin.do"
+fi
+echo "Login renren successfully!"
+if [ "$3" = "" ]; then
+	echo "get renren  musicbox ..."
+	wget --load-cookies cookies.txt --save-cookies cookies.txt --keep-session-cookies -O musicbox -o log "http://music.renren.com/musicbox"
+	echo "extract mp3 url ..."
+	grep "*.mp3" musicbox
+	mp3_src=`grep -o 'http:.*\.mp3' musicbox`
+	if [ "$mp3_src" != "" ];
+	then
+		wget --load-cookies cookies.txt --save-cookies cookies.txt --keep-session-cookies $mp3_src
+	else
+		echo "No music link is found!"
+	fi
+else
+	echo "specific music download is under construction!"
+	echo "try to get songId and download musicbox music.renren.com/musicbox?tab=1&autoPlay=true&songId=xxxxx"
 fi
